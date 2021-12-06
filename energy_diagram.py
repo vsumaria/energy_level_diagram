edbbedd7224d4b68bd48c73c36e6bf2ed70da697
vsumaria <33334729+vsumaria@users.noreply.github.com>
2021-12-06 12:47:21 -0800

    Update energy_diagram.py

--- energy_diagram.py
+++ energy_diagram.py
@@ -317,19 +317,10 @@ class ED:
 
 if __name__ == '__main__':
     a = ED()
-    a.add_level(0, 'Separated Reactants')
-    a.add_level(-5.4, 'mlC1')
-    a.add_level(-15.6, 'mlC2', 'last',)
-    a.add_level(28.5, 'mTS1', color='g')
-    a.add_level(-9.7, 'mCARB1')
-    a.add_level(-19.8, 'mCARB2', 'last')
-    a.add_level(20, 'mCARBX', 'last')
+    
+    a.add_level(0.3,top_text='17', bottom_text='abc', color='darkblue', top_fontsize=15, bottom_fontsize=6, top_color='r', bottom_color='b')
+    a.add_level(0.6,top_text='17', bottom_text='abc', color='darkblue', top_fontsize=15, bottom_fontsize=6, top_color='r', bottom_color='b')
+   
     a.add_link(0, 1, color='r')
-    a.add_link(0, 2)
-    a.add_link(2, 3, color='b')
-    a.add_link(1, 3)
-    a.add_link(3, 4, color='g')
-    a.add_link(3, 5)
-    a.add_link(0, 6)
     a.plot(show_IDs=True)
     plt.show()
